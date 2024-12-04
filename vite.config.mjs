63aead973500a4191229eaa2a677452865752299
steven <ste85213@gmail.com>
2024-12-04 18:16:57 +0800

    .

--- vite.config.mjs
+++ vite.config.mjs
@@ -14,6 +14,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: '/Ysphere/',
+  // base: './',
   base: './',
   plugins: [
     VueRouter(),
