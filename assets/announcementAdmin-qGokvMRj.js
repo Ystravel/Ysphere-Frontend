f8253b31c6f2501d09ca54239044c49434c2942e
Ystravel <steven_chen@ystravel.com.tw>
2024-12-05 10:06:40 +0000

    Deploying to gh-pages from @ Ystravel/Ysphere-Frontend@e5fa729c60951f83881c0d49311ba196ecf11770 🚀

--- assets/announcementAdmin-qGokvMRj.js
+++
@@ -1,1 +0,0 @@
-import{_ as n}from"./index-BcKy3KB6.js";const e={};function c(r,t){return" 公告管理頁面 "}const o=n(e,[["render",c]]);export{o as default};
--- assets/asset-C4E1GSUl.js
+++
@@ -1,1 +0,0 @@
-import{a2 as e,ak as a}from"./index-BcKy3KB6.js";const n={__name:"asset",setup(t){return(r,s)=>(e(),a("h2",null,"設備管理"))}};export{n as default};
